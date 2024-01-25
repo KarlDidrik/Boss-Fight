6dbb29badc206a94a8c7151a1b7b7997f7b8bfb0
KarlDidrik <karldodrik@gmail.com>
2024-01-25 13:45:22 +0100

    Add project files.

---
+++ Boss Fight/GameCharacter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boss_Fight
+{
+    internal class GameCharacter
+    {
+        public int Health { get; set; }
+        public int Strength { get; set; }
+        public int Stamina { get; set; }
+
+        public GameCharacter(int health, int strength, int stamina)
+        {
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+        }
+
+        public void Fight(GameCharacter opponent)
+        {
+            if (Stamina == 0)
+            {
+                Recharge();
+            }
+            else
+            {
+                int damage = opponent.Strength;
+                Health -= damage;
+
+                Console.WriteLine($"Enemy hit with {damage} damage, hero now has {Health} health left.");
+                Stamina -= 10;
+            }
+        }
+
+        public void Recharge()
+        {
+            Console.WriteLine("Not enough stamina, recharging...");
+            Stamina = 40;
+        }
+    }
+}
---
+++ Boss Fight/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Boss_Fight;
+
+GameCharacter hero = new GameCharacter(100, 20, 40);
+GameCharacter boss = new GameCharacter(400, 20, 10);
+
+while (hero.Health > 0 && boss.Health > 0)
+{
+    hero.Fight(boss);
+
+    if (boss.Health <= 0)
+    {
+        Console.WriteLine("Hero is the winner!");
+        break;
+    }
+
+    boss.Fight(hero);
+    if (hero.Health <= 0)
+    {
+        Console.WriteLine("Boss is the winner!");
+        break;
+    }
+}
